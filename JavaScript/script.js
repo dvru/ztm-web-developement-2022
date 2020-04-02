e3f8a88e73a39d33d8994163c943bde4bb578543
dvru <damvaru4@gmail.com>
2020-04-02 18:25:34 -0500

    JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -8,8 +8,38 @@
 //     console.log("You're Smart!");
 // }
 
-function sayHello() {
-    console.log("Hello");
-}
+// function sayHello() {
+//     console.log("Hello");
+// }
 
-sayHello();
+// sayHello();
+
+// var sayBye = function() {
+//     console.log("Bye");
+// }
+
+// sayBye();
+
+// function sing() {
+//     console.log("ahhh")
+//     console.log("teee")
+// }
+
+// sing();
+ 
+// function sing(song) {
+//     console.log(song);
+// }
+
+// sing("laaa dee daa");
+// sing("laaa dee daa");
+// sing("laaa dee daa");
+
+function multiply(a, b) {
+    if (a > 10 || b > 10) {
+        return "thats hard";
+    } else {
+        return a*b;
+        }
+    }
+multiply(5, 10);

18cc29451529f84915dba01c3ebe1ff175fc0477
dvru <damvaru4@gmail.com>
2020-04-02 17:58:18 -0500

    JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,9 +1,15 @@
-3 + 3;
+// 3 + 3;
+
+// // if (3+3 === 6) {
+// //     alert("You're Smart!");
+// // }
 
 // if (3+3 === 6) {
-//     alert("You're Smart!");
+//     console.log("You're Smart!");
 // }
 
-if (3+3 === 6) {
-    console.log("You're Smart!");
+function sayHello() {
+    console.log("Hello");
 }
+
+sayHello();

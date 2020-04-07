df2ea6203c0b30ba7b2d549cf98db2532f66cf55
dvru <damvaru4@gmail.com>
2020-04-07 18:56:21 -0500

    looping

--- JavaScript/script5.js
+++ JavaScript/script5.js
@@ -1,9 +1,17 @@
 // Loops
 
-var todo = [
+var todos = [
     "clean room",
     "brush",
     "exercise",
     "study javascript",
     "eat healthy"
 ];
+
+// for (var i=0; i < todos.length; i++) {
+//     console.log(todos[i] + "!");
+// }
+
+for (var i=0; i < todos.length; i++) {
+    todos[i] = todos[i] + "!"; 
+}

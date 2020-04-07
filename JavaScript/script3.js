07807ac96f3c2f40a1d996296f6f87b04e0d4299
dvru <damvaru4@gmail.com>
2020-04-07 15:25:37 -0500

    facebook app

---
+++ JavaScript/script3.js
@@ -0,0 +1,22 @@
+//Facebook APP
+
+var database = [
+    {
+        username: "d-money",
+        password: "money"
+    }
+];
+
+var newsfeed = [
+    {
+        username: "Bob",
+        timeline: "Hi, hello"
+    },
+    {
+        username: "Sally",
+        timeline: "Oh girl"
+    }
+];
+
+var userNamePrompt = prompt("What's your Username?");
+var passwordPrompt = prompt("What's your Password?")

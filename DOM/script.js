e3436e07b757cd4f7e7311829c4f059aa7fde24f
dvru <damvaru4@gmail.com>
2020-04-08 16:34:21 -0500

    added to the list

--- DOM/script.js
+++ DOM/script.js
@@ -1,1 +1,14 @@
-// var button = document.getElementsByTagName("button")[0];
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+button.addEventListener("click", function() {
+    // console.log(input.value);
+    if (input.value.length > 0) {
+        var li = document.createElement("li");
+
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = "";
+    }
+})

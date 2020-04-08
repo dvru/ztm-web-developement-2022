f6168ed499a24e3d521e42aba546be9429072cb7
dvru <damvaru4@gmail.com>
2020-04-08 16:42:48 -0500

    cleaned code

--- DOM/script.js
+++ DOM/script.js
@@ -2,25 +2,26 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-button.addEventListener("click", function() {
-    // console.log(input.value);
-    if (input.value.length > 0) {
-        var li = document.createElement("li");
+function inputLength() {
+    return input.value.length;
+}
 
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = "";
+function createListElement() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+
+button.addEventListener("click", function() {
+    if (inputLength() > 0) {
+        createListElement();
     }
 })
 
-
 input.addEventListener("keypress", function(event) {
-    if (input.value.length > 0 && event.keyCode === 13) {
-        var li = document.createElement("li");
-
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = "";
+    if (inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
     }
 })
 

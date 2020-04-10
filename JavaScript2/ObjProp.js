bcb8c9058372396d47c0cd28746d63785e66f02b
dvru <damvaru4@gmail.com>
2020-04-10 13:42:35 -0500

    obj properties

--- JavaScript2/ObjProp.js
+++ JavaScript2/ObjProp.js
@@ -4,5 +4,16 @@ const name = 'john snow';
 
 const obj = {
     [name]: 'hello',
-    ['ray' + 'smith']: 'hibye'
+    [1 + 2]: 'hibye'
 }
+
+const a = "Simon";
+const b = true;
+const c = {};
+
+const obj = { a, b, c }
+// const obj = {
+//     a: a,
+//     b: a,
+//     c: c
+// }

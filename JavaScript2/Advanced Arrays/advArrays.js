4ca56f0f4fa346f14205875ab69b39b1eab183a7
dvru <damvaru4@gmail.com>
2020-04-10 16:02:18 -0500

    Mapping

--- JavaScript2/Advanced Arrays/advArrays.js
+++ JavaScript2/Advanced Arrays/advArrays.js
@@ -1,33 +1,43 @@
 // Advanced Arrays
 
-// const array = [1, 2, 10, 16];
-// const newArray = array.forEach((num) => {
-//     num *2;
-// })
-// console.log(newArray);
-// ^^^^^ not storing the values.
-
-
-
+////////////////////////// forEach 
 //  storing the value within double, 
 // and push that within the empty array.
 // forEach loops over the array
 const array = [1, 2, 10, 16];
-
 const double = []
 const newArray = array.forEach((num) => {
     double.push(num * 2);
 })
-
 console.log('for each', double);
+
+
 
 
 /////////////// .map, .filter, .reduce
 
-// Map: always need to return something
-// map creates a new array
-const mapArray = array.map((num) => {
-    return num * 2;
-})
 
+////////////////////////// MAP: 
+// always need to return something
+// map creates a new array
+const mapArray = array.map(num => num * 2);
 console.log('map', mapArray);
+
+
+
+////////////////////////// FILTER: 
+// have to return
+/// filter the array, 
+// iterate one by one 
+// return 1 greater than 5
+const filterArray = array.filter(num => num === 5)
+console.log('filter', filterArray);
+
+
+////////////////////////// REDUCE: 
+// have to return value
+const reduceArray = array.reduce((accumulator, num) => {
+    return accumulator + num
+}, 5);
+console.log('reduce', reduceArray)
+

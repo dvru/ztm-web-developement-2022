ca00b7bbf65049aa6c3e57349cdd75f071d16b0f
dvru <damvaru4@gmail.com>
2020-04-10 17:02:17 -0500

    instantiation

--- JavaScript2/Advanced Objects/advObjects.js
+++ JavaScript2/Advanced Objects/advObjects.js
@@ -19,4 +19,25 @@ const object4 = {
     }
 }
 // instantiation
+class Player {
+    constructor(name, type){
+        console.log(this)
+        this.name = name;
+        this.type = type;
+    }
+    introduce() {
+        console.log(`Hi ${this.name}, I\'m a ${this.type}`)
+    }
+}
 
+class Wizard extends Player {
+    constructor(name, type) {
+        super(name, type)
+    }
+    play() {
+        console.log(`I am ${this.type}`);
+    }
+}
+
+const wizard1 = new Wizard('Bob', 'Healer');
+const wizard2 = new Wizard('Mel', 'Magic');

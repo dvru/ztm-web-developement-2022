2ad453fcfe24f08718eb29dd0d58a704390de5df
dvru <damvaru4@gmail.com>
2020-04-10 16:41:40 -0500

    reference type

---
+++ JavaScript2/Advanced Objects/advObjects.js
@@ -0,0 +1,19 @@
+// reference type
+
+var object1 = { value:10 };
+var object2 = object1;
+var object3 = { value:10 }
+
+object1 === object2
+true
+object1 === object3
+false
+
+object3 = object2
+{value: 10}
+object1 === object3
+true
+
+//context
+// instantiation
+

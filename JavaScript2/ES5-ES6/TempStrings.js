0a45edfde73cc8afa19134d633913bf9ffaf841d
dvru <damvaru4@gmail.com>
2020-04-10 15:30:40 -0500

    Template Strings

--- JavaScript2/ES5-ES6/TempStrings.js
+++ JavaScript2/ES5-ES6/TempStrings.js
@@ -8,8 +8,8 @@
 
 // const greetingBest = `Hello ${name} you seem to be ${age-10}. nice ${pet}`;
 
+
 function greet(name='', age=30, pet='cat') {
     return `Hello ${name} you seem to be ${age-10}. nice ${pet}`;
 }
-
 // need to provide arguments 

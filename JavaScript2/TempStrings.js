4144fec90ebbb18e442a0ccaa28205ead474c5b1
dvru <damvaru4@gmail.com>
2020-04-10 13:57:34 -0500

    template strings

--- JavaScript2/TempStrings.js
+++ JavaScript2/TempStrings.js
@@ -1,9 +1,15 @@
 // Template Strings
 
-const name = "bob";
-const age = 44;
-const pet = "cat";
+// const name = "bob";
+// const age = 44;
+// const pet = "cat";
 
 // const greeting = "Hello " + name + " you seem to be doing" + greeting  + "!"
 
-const greetingBest = `Hello ${name} you seem to be ${age-10}. nice ${pet}`;
+// const greetingBest = `Hello ${name} you seem to be ${age-10}. nice ${pet}`;
+
+function greet(name='', age=30, pet='cat') {
+    return `Hello ${name} you seem to be ${age-10}. nice ${pet}`;
+}
+
+// need to provide arguments 

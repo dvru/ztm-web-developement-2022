9828bae6d532d027de23b8849902f9dbd5ceda6c
dvru <damvaru4@gmail.com>
2020-04-10 16:51:48 -0500

    context

--- JavaScript2/Advanced Objects/advObjects.js
+++ JavaScript2/Advanced Objects/advObjects.js
@@ -1,19 +1,22 @@
 // reference type
-
 var object1 = { value:10 };
 var object2 = object1;
 var object3 = { value:10 }
+// object1 === object2
+// true
+// object1 === object3
+// false
+// object3 = object2
+// {value: 10}
+// object1 === object3
+// true
 
-object1 === object2
-true
-object1 === object3
-false
 
-object3 = object2
-{value: 10}
-object1 === object3
-true
-
-//context
+// //context vs scope
+const object4 = {
+    a: function() {
+        console.log(this);
+    }
+}
 // instantiation
 

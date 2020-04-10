09283605065fccca8932242c9b43951cd4f9f187
dvru <damvaru4@gmail.com>
2020-04-10 13:54:42 -0500

    template strings

---
+++ JavaScript2/TempStrings.js
@@ -0,0 +1,9 @@
+// Template Strings
+
+const name = "bob";
+const age = 44;
+const pet = "cat";
+
+// const greeting = "Hello " + name + " you seem to be doing" + greeting  + "!"
+
+const greetingBest = `Hello ${name} you seem to be ${age-10}. nice ${pet}`;

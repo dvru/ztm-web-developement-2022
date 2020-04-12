a25fed871c6fe2dc29ace35f60dd02b6d47ce092
dvru <damvaru4@gmail.com>
2020-04-12 14:39:30 -0500

    advanced objects exercise

--- JavaScript2/Advanced Objects/advObjectseExercise.js
+++ JavaScript2/Advanced Objects/advObjectseExercise.js
@@ -11,5 +11,6 @@ const object4 = { a: 5};
 object1.a = 4;
 
 
+
 //#3 create two classes: an Animal class and a Mamal class. 
 // create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 

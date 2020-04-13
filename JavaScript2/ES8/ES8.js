ab0bc9cbc807f426da299b6f5ed1f63f01a47c76
dvru <damvaru4@gmail.com>
2020-04-13 14:24:59 -0500

    ES8

--- JavaScript2/ES8/ES8.js
+++ JavaScript2/ES8/ES8.js
@@ -12,6 +12,32 @@
 const fun = (a,b,c,d,) => {
     console.log(a);
 }
-
 fun(1,2,3,4,)
 
+
+//OBJECTS
+Object.keys
+Object.values
+Object.entries
+
+
+let obj = {
+    username0: 'Santa',
+    username1: 'Rudolf',
+    username2: 'Grinch'
+}
+
+// returns the keys
+Object.keys(obj).forEach((key, index) => {
+    console.log(key, obj[key]);
+}) 
+
+// returns the values
+Object.values(obj).forEach(value => {
+    console.log(value);
+})
+
+// returns the array of the both key and value pairs
+Object.entries(obj).forEach(value => {
+    console.log(value);
+})

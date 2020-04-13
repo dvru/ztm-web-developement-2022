c8f135eae1e217d8f2a7af13de2336b760f47906
dvru <damvaru4@gmail.com>
2020-04-13 14:38:38 -0500

    ES8 exercise

--- JavaScript2/ES8/ES8exercise.js
+++ JavaScript2/ES8/ES8exercise.js
@@ -15,6 +15,8 @@ console.log(startLine);
 console.log(turtle);
 console.log(rabbit);
 
+turtle = turtle.padStart(8);
+rabbit = rabbit.padStart(8);
 
 // #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
 // Read about what the second parameter does in padEnd and padStart

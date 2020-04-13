a327dc553650d140b1a74f618b43dd6ec0f41a3b
dvru <damvaru4@gmail.com>
2020-04-13 16:16:10 -0500

    Advanced Loops exercise

--- JavaScript2/Advanced Loops/loopsExcercise.js
+++ JavaScript2/Advanced Loops/loopsExcercise.js
@@ -32,17 +32,42 @@ const array = [-1,0,3,100, 99, 2, 99] // should return 100
 const array2 = ['a', 3, 4, 2] // should return 4
 const array3 = [] // should return 0
 
-function biggestNumberInArray(arr) {
+const array = [-1,0,3,100, 99, 2, 99] // should return 100
+const array2 = ['a', 3, 4, 2] // should return 4
+const array3 = [] // should return 0
 
-}
+function biggestNumberInArray(arr) {
+    let highest = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (highest < arr[i]) {
+        highest = arr[i];
+      }
+    }
+    return highest
+  }
 
 function biggestNumberInArray2(arr) {
-
-}
-
-function biggestNumberInArray3(arr) {
-
-}
+    let highest = 0;
+    arr.forEach(item => {
+      if (highest < item) {
+        highest = item;
+      }
+    })
+    return highest;
+  }
+  
+  function biggestNumberInArray3(arr) {
+    let highest = 0;
+    for (item of arr) {
+      if (highest < item) {
+        highest = item;
+      }
+    }
+    return highest;
+  }
+  
+  
+  biggestNumberInArray3(array3)
 
 
 // Question #2:

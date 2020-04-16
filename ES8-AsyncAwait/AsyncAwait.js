7605c4065c291a5afb6e6711e982af78788f9d23
dvru <damvaru4@gmail.com>
2020-04-16 17:26:51 -0500

    Async Await

--- ES8-AsyncAwait/AsyncAwait.js
+++ ES8-AsyncAwait/AsyncAwait.js
@@ -1,13 +1,28 @@
 // ASYNC AWAIT
 
+
+//fetch
 // movePlayer(100, 'Left')
 //     .then(() => movePlayer(400, 'Left'))
 //     .then(() => movePlayer(10, 'Right'))
 //     .then(() => movePlayer(300, 'Left'))
-
+//async
 async function playerStart() {
-    const firstMove = await movePlayer(100, 'Left'); //pause
-    await movePlayer(400, 'Left'); //pause
+    const first = await movePlayer(100, 'Left'); //pause
+    const second = await movePlayer(400, 'Left'); //pause
     await movePlayer(10, 'Right'); //pause
     await movePlayer(300, 'Left'); //pause
+}
+
+
+
+//fetch
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(resp => resp.json()
+//     .then(console.log))
+//async
+async function fetchUsers() {
+    const resp = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await resp.json()
+    console.log(data);
 }

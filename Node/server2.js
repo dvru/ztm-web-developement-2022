95af79050ceba2d5d63b997e4d458fbc6a04136b
dvru <damvaru4@gmail.com>
2020-04-21 17:14:25 -0500

    middleware

--- Node/server2.js
+++ Node/server2.js
@@ -2,21 +2,38 @@ const express = require('express');
 
 const app = express();
 
-
+app.use((req, res, next) => {
+    console.log('<h1>Helllo</h1>')
+    next();
+})
 app.get('/', (req, res) => {
-    res.send("getting root");
-})
-
-app.get('/profile', (req, res) => {
-    res.send("getting profile");
-})
-
-app.post('/profile', (req, res) => {
-    const user = {
-        name: "sally",
-        hobby: "eating"
-    }
-    res.send(user);
+    res.send("testtest");
 })
 
 app.listen(3002)
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/', (req, res) => {
+//     res.send("getting root");
+// })
+
+// app.get('/profile', (req, res) => {
+//     res.send("getting profile");
+// })
+
+// app.post('/profile', (req, res) => {
+//     const user = {
+//         name: "sally",
+//         hobby: "eating"
+//     }
+//     res.send(user);
+// })

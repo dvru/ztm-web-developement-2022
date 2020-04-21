2c30d2eea0ba37ec2ed58a90956c8151a80fd6ac
dvru <damvaru4@gmail.com>
2020-04-21 16:52:42 -0500

    changes

---
+++ Node/script.js
@@ -0,0 +1,14 @@
+const script2 = require ('./script2.js')
+
+const a = script2.largeNumber;
+const b = 10;
+
+console.log(a + b);
+
+
+
+
+// setTimeout(() => {
+//     console.log(a + b);
+// }, 3000)
+

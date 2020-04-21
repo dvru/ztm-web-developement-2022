0bf4b5516915c20b40d720ef9c8399bf22efb5f8
dvru <damvaru4@gmail.com>
2020-04-21 16:16:58 -0500

    added script2 file

--- Node/script.js
+++ Node/script.js
@@ -1,7 +1,9 @@
-const a = 4;
+ const script2 = require ('./script2.js')
+
+const a = script2.largeNumber;
 const b = 5;
 
-// console.log(__dirname);
+console.log(a + b);
 
 // setTimeout(() => {
 //     console.log(a + b);

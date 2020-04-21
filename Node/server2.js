1167c464173ac97045d4e6333dab4d919dddd354
dvru <damvaru4@gmail.com>
2020-04-21 17:32:20 -0500

    added body-parser

--- Node/server2.js
+++ Node/server2.js
@@ -20,20 +20,3 @@ app.listen(3002)
 
 
 
-
-
-// app.get('/', (req, res) => {
-//     res.send("getting root");
-// })
-
-// app.get('/profile', (req, res) => {
-//     res.send("getting profile");
-// })
-
-// app.post('/profile', (req, res) => {
-//     const user = {
-//         name: "sally",
-//         hobby: "eating"
-//     }
-//     res.send(user);
-// })

09178d6a0d3d24214d26bbe5ac4bca32c1b1e503
dvru <damvaru4@gmail.com>
2020-04-21 16:51:18 -0500

    express

--- JavaScript2/ES5-ES6/arrowFunctions.js
+++
@@ -1,8 +0,0 @@
-// arrow functions
-
-
-function add(a, b) {
-    return a + b;
-}
-
-const add2 = (a, b) => a + b;
--- JavaScript2/ES5-ES6/symbol.js
+++
@@ -1,8 +0,0 @@
-//Symbols
-
-let sym1 = Symbol();
-let sym2 = Symbol('foo');
-let sym3 = Symbol('foo');
-
-// sym2 === sym3
-// false

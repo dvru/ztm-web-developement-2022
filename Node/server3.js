245ee57735f384f935b793374fc5388e9dae3a37
dvru <damvaru4@gmail.com>
2020-04-21 21:08:41 -0500

    added public folder

--- Node/server3.js
+++ Node/server3.js
@@ -5,17 +5,28 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-app.get('/', (req, res) => {
-    res.send("getting root");
-})
 
-app.get('/profile', (req, res) => {
-    res.send("getting profile");
-})
-
-app.post('/profile', (req, res) => {
-    console.log(req.body)
-    res.send('Success');
+app.get('/:id', (req, res) => {
+    console.log(req.params)
+    res.status(404).send("not found");
 })
 
 app.listen(3002)
+
+
+
+
+
+
+
+
+
+// app.get('/profile', (req, res) => {
+//     res.send("getting profile");
+// })
+
+// app.post('/profile', (req, res) => {
+//     console.log(req.body)
+//     res.send('Success');
+// })
+
